a3572d3c22ee3b8dd5d3e533fbdb1455c4da0d67
Matheus Mendes <msmmendes211@gmail.com>
2024-06-02 19:56:11 -0300

    Add onboarding controller classes

---
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -0,0 +1,54 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+import '../../screens/login/login.dart';
+
+class OnboardingController extends GetxController {
+  static OnboardingController get instance => Get.find();
+
+  /// Variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+  bool isLastPage = false;
+  bool isSkipped = false;
+  bool isDarkMode = false;
+  bool isDarkModeOn = false;
+  bool isDarkModeOff = false;
+  bool isDarkModeAuto = false;
+
+  /// Update current index when page scroll
+  void updatePageIndicator(index) => currentPageIndex.value = index;
+
+  /// Jump to the specific dot selected page
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index);
+  }
+
+  /// Update current index and jump to next page
+  void nextPage() {
+    if (currentPageIndex.value == 2) {
+      Get.to(LoginScreen());
+    } else {
+      currentPageIndex.value++;
+      pageController.jumpToPage(currentPageIndex.value);
+      // int page = currentPageIndex.value++;
+      // pageController.jumpToPage(page);
+    }
+  }
+
+  /// Update current index and jump to previous page
+  void previousPage() {
+  }
+
+  /// Update current index and skip to last page
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
+
+
+
+
+
+}
