9a06e27f52c9391f551f9279a7bcb38d6ffede0a
Matheus Mendes <msmmendes211@gmail.com>
2024-06-02 19:55:22 -0300

    Add main and app classes

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:msm_gym_app/util/theme/theme.dart';
+
+import 'features/authentication/screens/onboarding/onboarding.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      home: const OnboardingScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+void main() {
+
+  // Todo: Add Widgets Biding
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
+  /// Todo: Init App
+  /// Todo: Init Routes
+
+  runApp(const App());
+}
+
