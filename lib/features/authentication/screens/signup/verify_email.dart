afd5928aeaa62925e69a7c8de314b876598753d8
Matheus Mendes <msmmendes211@gmail.com>
2024-06-07 14:41:57 -0300

    Update VerifyEmailScreen class

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../../common/widgets/login_signup/verification_code_input.dart';
 import '../../../../util/constants/image_strings.dart'; // Certifique-se que o caminho está correto
 import '../../../../util/constants/sizes.dart';
 import '../../../../util/constants/texts.dart';
@@ -25,13 +26,14 @@ Widget _verifyEmail(BuildContext context) {
         children: [
           Center(
             child: Image(
-              width: THelperFunctions.screenWidth() * 0.8,
-              height: THelperFunctions.screenHeight() * 0.6,
+              width: THelperFunctions.screenWidth() * 0.7,
+              height: THelperFunctions.screenHeight() * 0.4,
               image: const AssetImage(
                 TImages.verifySendEmail,
               ),
             ),
           ),
+          const SizedBox(height: TSizes.spacingBetweenSections),
           Text(
             TTexts.confirmEmailTitle,
             style: Theme.of(context).textTheme.headlineMedium,
@@ -45,7 +47,7 @@ Widget _verifyEmail(BuildContext context) {
           ),
           const SizedBox(height: TSizes.spacingBetweenSections),
           const VerificationCodeInput(),
-          const SizedBox(height: TSizes.spacingBetweenSections),
+          const SizedBox(height: TSizes.spacingBetweenSections * 2),
           SizedBox(
             width: double.infinity,
             child: ElevatedButton(
@@ -53,75 +55,14 @@ Widget _verifyEmail(BuildContext context) {
               child: const Text(TTexts.keepGoing),
             ),
           ),
+          const SizedBox(height: TSizes.spacingBetweenSections),
+          TextButton(
+            onPressed: () => Get.to(() => const AccountCreatedSuccessScreen()),
+            child: const Text(TTexts.resendEmail),
+          ),
         ],
       ),
     ),
   );
 }
 
-class VerificationCodeInput extends StatefulWidget {
-  const VerificationCodeInput({super.key});
-
-  @override
-  State<VerificationCodeInput> createState() => _VerificationCodeInputState();
-}
-
-class _VerificationCodeInputState extends State<VerificationCodeInput> {
-  final int codeLength = 6;
-  final List<TextEditingController> controllers = [];
-  final List<FocusNode> focusNodes = [];
-
-  @override
-  void initState() {
-    super.initState();
-    for (int i = 0; i < codeLength; i++) {
-      controllers.add(TextEditingController());
-      focusNodes.add(FocusNode());
-    }
-  }
-
-  @override
-  void dispose() {
-    for (int i = 0; i < codeLength; i++) {
-      controllers[i].dispose();
-      focusNodes[i].dispose();
-    }
-    super.dispose();
-  }
-
-  void _nextField(String value, int index) {
-    if (value.length == 1) {
-      if (index < codeLength - 1) {
-        FocusScope.of(context).requestFocus(focusNodes[index + 1]);
-      } else {
-        focusNodes[index].unfocus();
-      }
-    } else if (value.isEmpty && index > 0) {
-      FocusScope.of(context).requestFocus(focusNodes[index - 1]);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: List.generate(codeLength, (index) {
-        return SizedBox(
-          width: 40,
-          height: 40,
-          child: TextField(
-            controller: controllers[index],
-            focusNode: focusNodes[index],
-            maxLength: 1,
-            textAlign: TextAlign.center,
-            decoration: const InputDecoration(
-              counterText: "",
-            ),
-            keyboardType: TextInputType.text,
-            onChanged: (value) => _nextField(value, index),
-          ),
-        );
-      }),
-    );
-  }
-}
